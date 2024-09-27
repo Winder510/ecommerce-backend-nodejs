dd428331167fb35b60f47033b6d2be9410363f7c
HoangGia Phong <144602252+Winder510@users.noreply.github.com>
2024-09-27 14:39:01 +0700

    add

---
+++ src/routes/notification/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import {
+    authenticationV2
+} from '../../auth/authUtils.js';
+import {
+    asyncErrorHandler
+} from '../../helpers/asyncHandler.js';
+import notificationController from '../../controllers/notification.controller.js';
+const router = express.Router()
+
+// not login
+
+router.use(authenticationV2);
+// login
+router.get('', asyncErrorHandler(notificationController.getListNoti))
+
+
+export default router
