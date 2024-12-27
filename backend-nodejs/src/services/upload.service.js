aaed9090b30f06e4739f14ef9a86f4fedbbfb796
HoangGia Phong <144602252+Winder510@users.noreply.github.com>
2024-12-28 01:25:23 +0700

    fix

--- backend-nodejs/src/services/upload.service.js
+++ backend-nodejs/src/services/upload.service.js
@@ -2,6 +2,22 @@ import {
     cloudinary
 } from '../configs/cloudinary.config.js';
 
+const uploadImageFromUrl = async () => {
+    try {
+        const urlImage =
+            'https://down-vn.img.susercontent.com/file/vn-11134207-7ras8-m0i5vom5u86lac@resize_w900_nl.webp';
+        const folderName = 'product',
+            newFileName = 'testdemo';
+
+        const result = await cloudinary.uploader.upload(urlImage, {
+            productId: newFileName,
+            folder: folderName,
+        });
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 const uploadImageFromLocal = async ({
     path,
